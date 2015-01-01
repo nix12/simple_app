daf5e608c8adb322aa899ce1e52d86480e4285b4
Mario Goodwin <mr.goodwin@outlook.com>
2015-01-01 18:40:00 -0500

    working through

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update] 
+
   def new
   	@user = User.new
   end
@@ -10,11 +12,25 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		flash[:success] = "Welcome to the Rails Sample App"
   		redirect_to @user
   	else
   		render 'new'
   	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #successful update
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -23,4 +39,11 @@ class UsersController < ApplicationController
 	  	params.require(:user).permit(:name, :email, :password,
 	  									:password_confirmation)
 	  end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
@@ -8,6 +9,12 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   
   get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
   
